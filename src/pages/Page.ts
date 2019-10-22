c27f7cb7921a28413752759636887f4db470b2ba
Sat <satraj.sehmi@dvcorporate.com>
2019-10-22 09:38:23 +0100

    Added types for webdiverio, updated base page class to include helper to get pagesource for SSR

--- src/pages/Page.ts
+++ src/pages/Page.ts
@@ -1,5 +1,21 @@
 export default abstract class Page {
-    open (path) {
+    public open (path: string): void {
         browser.url(path)
+    }
+
+    /**
+     * For SSR rendered apps the client can render different content to the server
+     * This allows the view source of the page to be inspected to verify its the same as the
+     * DOM when the clien renders
+     */
+    public getTextFromSource(selector: string): string {
+        const pageSource = browser.getPageSource();
+
+        const text = browser.execute((pagesource: string, queryElement: string) => {
+            let documentFragment = document.createRange().createContextualFragment(pagesource);
+            return documentFragment.querySelector(queryElement).textContent;
+        }, pageSource, selector);
+
+        return text;
     }
 }
--- src/stepDefinitions/homepage-search.steps.ts
+++ src/stepDefinitions/homepage-search.steps.ts
@@ -1,22 +1,14 @@
 import { assert } from 'chai';
 import { binding, given, when, then } from 'cucumber-tsflow';
-import Homepage from '../pages/Homepage';
+import homepage from '../pages/Homepage';
 
 @binding()
 export class HomepageSearchSteps {
     @given(/^I am on the search page$/)
     public givenOnHomepage() {
-        Homepage.open();
+        homepage.open();
         const title = browser.getTitle();
-
-        const pageSource = browser.getPageSource();
-
-        const seoTitle = browser.execute(pageSource => {
-            let frag = document
-                .createRange()
-                .createContextualFragment(pageSource);
-            return frag.querySelector('title').textContent;
-        }, pageSource);
+        const seoTitle = homepage.getTextFromSource('title');
 
         assert.equal(seoTitle, 'Google');
         assert.equal(title, 'Google');
@@ -24,18 +16,18 @@ export class HomepageSearchSteps {
 
     @when(/^I enter "([^"]*)" into the search box$/)
     public whenISearch(arg1) {
-        Homepage.enterText(arg1);
-        const searchText = Homepage.searchInput.getValue();
+        homepage.enterText(arg1);
+        const searchText = homepage.searchInput.getValue();
         assert.equal(searchText, arg1);
     }
 
     @when(/^I click the search button$/)
     public whenSearchClicked() {
-        Homepage.search();
+        homepage.search();
     }
 
     @then(/^I should see a list of search results$/)
     public resultsShouldShow() {
-        assert.isTrue(Homepage.isSearched());
+        assert.isTrue(homepage.isSearched());
     }
 }
